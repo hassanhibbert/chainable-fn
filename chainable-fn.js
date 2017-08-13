3831edfcc4e26f42176270e9330a24c2168e7d6a
H2-CODE\Hassan <hassanhibbert@gmail.com>
2017-08-13 13:26:53 -0400

    initial commit and project setup

---
+++ chainable-fn.js
@@ -0,0 +1,20 @@
+var Chainable = (function () {
+  var currentValue;
+
+  var publicAPI = {
+    init: init,
+    next: next,
+    value: value
+  };
+
+  return publicAPI;
+  ////////////////
+
+  function init() {}
+  function next() {}
+  function value() {}
+
+})();
+
+
+
