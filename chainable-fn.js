267bf5da8f2917428000924de45a921977f7bfdd
Hassan Hibbert <hhibbert@fullsail.com>
2017-08-14 11:59:39 -0400

    remove example from script since there is one in the readme

--- chainable-fn.js
+++ chainable-fn.js
@@ -22,21 +22,3 @@
   root.Chainable = root.Chainable || setupChainable;
 
 })(window);
-
-// Example functions
-var add = function (a, b) { return a + b };
-var multiply = function (a, b) { return a * b };
-var subtract = function (a, b) { return a - b };
-var divide = function (a, b) { return a / b };
-var square = function (a) { return a * a };
-
-// Usage
-var value = Chainable(4)
-  .next(add, 5)
-  .next(subtract, 3)
-  .next(multiply, 10)
-  .next(divide, 2)
-  .next(square)
-  .value();
-
-console.log(value); // 900
